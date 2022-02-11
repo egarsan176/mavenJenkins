6dee150ddad097c287f9d03247ca7f7d0d332d23
Estefanía García <73167852+egarsan176@users.noreply.github.com>
2022-02-11 10:34:04 +0100

    añadiendo test fallido

--- src/test/java/com/example/demo/NuevaVersionBdApplicationTests.java
+++ src/test/java/com/example/demo/NuevaVersionBdApplicationTests.java
@@ -1,18 +1,32 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.model.Usuario;
+
 @SpringBootTest
 class NuevaVersionBdApplicationTests {
+	
+	private Usuario usuario;
+	
+	@BeforeEach
+	public void init() {
+		this.usuario = new Usuario("estefgar");
+	}
+	
+	@Test
+	void testGetUserName() {
+		assertEquals("estef", usuario.getUserName());
+	}
 
 	@Test
 	void contextLoads() {
 	}
 	
-	@Test
-	void testFallido() {
-		assertTrue(false);
-	}
+	
 
 }
